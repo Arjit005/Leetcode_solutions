e2f4c6d79ed705aac424090c9a01d89ab272ee41
Arjit Katiyar <katiyarkarn295@gmail.com>
2026-08-01 01:48:12 +0530

    Time: 0 ms (100%), Space: 19.4 MB (34.25%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        # first bracket must be "("
+        # At any point while building the string, the number of ) can never be greater than the number of (.
+        # we need something to store results
+        output=[]
+        # we use backtrackin 
+        # defining dfs
+        def dfs(current,open_bracket,close_bracket): # current is string of brackets
+            # base case
+            if len(current)==2*n:
+                output.append(current)
+                return
+            if open_bracket<n:
+                dfs(current+"(",open_bracket+1,close_bracket)
+            if close_bracket <open_bracket:
+                dfs(current+")",open_bracket,close_bracket+1)
+        dfs("",0,0)
+        return output
+            
